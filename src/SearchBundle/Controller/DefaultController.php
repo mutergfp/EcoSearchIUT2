031441c011f794948dfa81c8a5a7a75d214a2ec7
Maël Gaonach <mael@gaonach.com>
2017-12-12 13:45:53 +0100

    Implémentation de la tolérence aux erreurs dans la recherche

--- src/SearchBundle/Controller/DefaultController.php
+++ src/SearchBundle/Controller/DefaultController.php
@@ -16,21 +16,32 @@ class DefaultController extends Controller
         $tags_string = explode(" ", urldecode($research));
         $repository_tag = $this->getDoctrine()->getRepository('SearchBundle:Tag');
         $tags = array();
+        $tags_found = $repository_tag->findAll();
         foreach ($tags_string as $tag_string)
         {
-            $tag = $repository_tag->findOneBy(array(
-                'nom' => $tag_string
-            ));
-            if($tag != null){
-                $tags[] = $tag;
+            $percent_max=0.0;
+            $tag_found=null;
+            foreach ($tags_found as $tag) {
+                $percent = 0.0;
+                similar_text($tag->getNom(),$tag_string,$percent);
+                if($percent>$percent_max && $percent>70.0){
+                    $tag_found=$tag;
+                    $percent_max=$percent;
+                }
+            }
+            if($tag_found != null){
+                $tags[] = $tag_found;
+                //$tag_found->setNom($tag_found->getNom().' ('.round($percent_max).'%)');
             }
         }
+
         $repository_produit = $this->getDoctrine()->getRepository('SearchBundle:Produit');
         $produits = $repository_produit->getByTags($tags);
 
         return $this->render('SearchBundle:Default:search.html.twig', array(
             "produits" => $produits,
-            "recherche" => urldecode($research)
+            "recherche" => urldecode($research),
+            "tags"=>$tags
         ));
         //return new Response(var_dump($produits));
     }
