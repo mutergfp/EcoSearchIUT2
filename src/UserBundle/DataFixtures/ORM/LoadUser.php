584377f53fd19871f0abdfbc948d849a39b653be
TerraxX <aubin.dubreuil@hotmail.fr>
2018-01-16 10:19:30 +0100

    Modification du UserBundle

--- src/UserBundle/DataFixtures/ORM/LoadUser.php
+++ src/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -3,35 +3,55 @@
 
 namespace UserBundle\DataFixtures\ORM;
 
-use Doctrine\Common\DataFixtures\FixtureInterface;
+use Acme\AppBundle\Entity\User;
+use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use UserBundle\Entity\User;
-
-class LoadUser implements FixtureInterface
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+ 
+class LoadUser extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
 {
-  public function load(ObjectManager $manager)
-  {
-    // Les noms d'utilisateurs à créer
-    $listNames = array('Aubin', 'Luc', 'Mael', 'Antonin', 'Lucas');
-
-    foreach ($listNames as $name) {
-      // On crée l'utilisateur
-      $user = new User;
-
-      // Le nom d'utilisateur et le mot de passe sont identiques pour l'instant
-      $user->setUsername($name);
-      $user->setPassword($name);
-
-      // On ne se sert pas du sel pour l'instant
-      $user->setSalt('');
-      // On définit uniquement le role ROLE_USER qui est le role de base
-      $user->setRoles(array('ROLE_USER'));
-
-      // On le persiste
-      $manager->persist($user);
-    }
-
-    // On déclenche l'enregistrement
-    $manager->flush();
-  }
+ 
+ /**
+ * @var ContainerInterface
+ */
+ private $container;
+ 
+ /**
+ * Load data fixtures with the passed EntityManager
+ * @param ObjectManager $manager
+ */
+ public function load(ObjectManager $manager)
+ {
+   $userManager = $this->container->get('fos_user.user_manager');
+ 
+   $user = $userManager->createUser();
+   $user->setUsername('utilisateur');
+   $user->setEmail('utilisateur@ecosearch.fr');
+   $user->setPlainPassword('utilisateur');
+   $user->setEnabled(true);
+   $user->addRole('ROLE_USER');
+   $this->addReference('user-admin', $user);
+   $userManager->updateUser($user);
+ }
+ 
+ /**
+ * Sets the container.
+ * @param ContainerInterface|null $container A ContainerInterface instance or null
+ */
+ public function setContainer(ContainerInterface $container = null)
+ {
+   $this->container = $container;
+ }
+ 
+  /**
+ * Get the order of this fixture
+ * @return integer
+ */
+ public function getOrder()
+ {
+   return 2;
+ }
+ 
 }
